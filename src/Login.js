9710e3fc8230018296ce349aa485006c36aa31c6
Abdul Hakeem <haq9966@gmail.com>
2021-08-26 10:10:36 +0530

    added style to the login page

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 function Login (){
     
     return(
         <div className="login">
+            <Link>
+                <img className="login__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt=""/>
+            </Link>
             <div className="login__container">
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="email"/>
+                    <input type="email" />
                     <h5>Password</h5>
-                    <input type="password"/>
-                    <button type="submit" className="login__signInbutton">Sign In</button>
+                    <input type="password" />
+                    <button type="submit" className="login__signInButton">Sign In</button>
                 </form>
                 <p>By singing in you agree to Amazon's terms and conditions.</p>
+                <button className="login__registerButton">Create your Amazon account</button>
             </div>
         </div>
     )
