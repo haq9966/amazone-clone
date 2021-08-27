8f3f88cb830d98e85a1b46d0040b0e0ef8cbbcf2
Abdul Hakeem <haq9966@gmail.com>
2021-08-27 13:12:20 +0530

    working on footer styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
 import Login from './Login'
 import Home from './Home'
 import Checkout from './Checkout'
+import Footer from './Footer';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/"> 
           <Header/>
           <Home/>
+          <Footer/>
         </Route>
       </Switch>
       </div>
